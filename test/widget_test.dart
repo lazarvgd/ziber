f9d97224eb062d37873e33ae0a2ce741eb01ff4d
lazar <lazarvgd@gmail.com>
2020-02-08 13:23:11 +0100

    commented old code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,7 +10,6 @@ import 'package:ziber/main.dart';
 
 void main() {
   testWidgets('Checking if hello world shows up', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
     expect(find.text('Hello World!'), findsOneWidget);
   });
